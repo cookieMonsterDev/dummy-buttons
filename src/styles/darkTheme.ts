6dc151c1d6733a23b8f490ddc19987bfdd6c63df
It-is-a-piece-of-cake <mykhailo.toporkov@gmail.com>
2022-06-17 20:57:01 +0300

    feat: update themes for settings

--- src/styles/darkTheme.ts
+++ src/styles/darkTheme.ts
@@ -1,9 +1,17 @@
 interface DarkTheme {
   globalColor: string;
   globalBackground: string;
+  textColorPrimary: string;
+  textColorSecondary: string;
+  toggleBackground: string;
+  toggleBackgroundActive: string;
 };
 
 export const DarkTheme = {
-  globalColor: 'red',
-  globalBackground: 'black',
+  globalColor: '',
+  globalBackground: '#000000',
+  textColorPrimary: '#FF9500',
+  textColorSecondary: '#FF9500',
+  toggleBackground: '#B3B3B3',
+  toggleBackgroundActive: '#FF9500',
 };
--- src/styles/lightTheme.ts
+++ src/styles/lightTheme.ts
@@ -1,9 +1,17 @@
 interface LightTheme {
-  globalColor: string;
+  lobalColor: string;
   globalBackground: string;
+  textColorPrimary: string;
+  textColorSecondary: string;
+  toggleBackground: string;
+  toggleBackgroundActive: string;
 };
 
 export const LightTheme = {
-  globalColor: 'blue',
-  globalBackground: 'white',
+  globalColor: '',
+  globalBackground: '#ECECEC',
+  textColorPrimary: '#333333',
+  textColorSecondary: '#333333',
+  toggleBackground: '#B3B3B3',
+  toggleBackgroundActive: '#00CC00',
 };
